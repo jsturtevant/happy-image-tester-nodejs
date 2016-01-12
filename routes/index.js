487b4fb534aeb102a85a589b6c80d6c20766e06a
James Sturtevant <jsturtevant@gmail.com>
2016-01-12 13:29:40 -0500

    added oxford code to do processing

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var oxford = require('project-oxford');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +12,17 @@ router.get('/result', function(req, res, next) {
 });
 
 router.post('/upload', function(req, res, next) {
-  res.render('result', { title: 'Express' });
+    var client = new oxford.Client('b38409352b284f459b5e60a3e9eb4fcd');
+    
+    client.emotion.analyzeEmotion({
+        url: 'http://www.jamessturtevant.com/assets/profile-low-res.jpg',
+    }).then(function (response) {
+        console.log(response);
+        
+        res.render('result', { title: 'Express' });
+    });  
+    
+  
 });
 
 module.exports = router;
