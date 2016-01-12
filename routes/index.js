24fd7c010630a0060a7a910fd314f6b09baf3bdb
James Sturtevant <jsturtevant@gmail.com>
2016-01-12 13:06:41 -0500

    setup result page.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/result', function(req, res, next) {
+  res.render('result', { title: 'Express' });
+});
+
 module.exports = router;
