c8a5e412525595c75168b53e345d0792b625cd02
James Sturtevant <jsturtevant@gmail.com>
2016-01-12 21:57:47 -0500

    added ability to add mutlple faces

--- routes/index.js
+++ routes/index.js
@@ -6,45 +6,52 @@ var multer = require('multer');
 var upload = multer({ dest: 'uploads/' });
 var oxford = require('project-oxford');
 var fs = require('fs')
-  , gm = require('gm');
+    , gm = require('gm');
 
 
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/result', function(req, res, next) {
+router.get('/result', function (req, res, next) {
     res.render('result', { title: 'Express' });
 });
 
-router.post('/upload', upload.single('thumbnail'), function(req, res, next) {
+router.post('/upload', upload.single('thumbnail'), function (req, res, next) {
     var client = new oxford.Client('b38409352b284f459b5e60a3e9eb4fcd');
 
     client.emotion.analyzeEmotion({
         path: req.file.path,
     }).then(function (response) {
         console.log(response);
-      
-        gm(req.file.path)
-            .stroke("red", 7)
-            .fill("#ffffffbb")
-            .drawRectangle(60, 10, 70, 20)
-            .write('uploads/' + 'modified' + '.jpg', function (err) {
-                if (err) console.log(err);
-                
-         
-               var modifiedimage = base64_encode('uploads/' + 'modified' + '.jpg');
-            
-                res.render('result', { modified: modifiedimage });
-            });
-       
+
+
+        var draw = gm(req.file.path);
+        response.forEach(function (element) {
+            var x = element.faceRectangle.left;
+            var y = element.faceRectangle.top;
+            var x1 = element.faceRectangle.left + element.faceRectangle.width;
+            var y1 = element.faceRectangle.top + element.faceRectangle.height;
+
+            draw.stroke("red", 2)
+                .fill("#ffffffff")  //transparent box
+                .drawRectangle(x, y, x1, y1)
+        }, this);
+
+        draw.write('uploads/' + 'modified' + '.jpg', function (err) {
+            if (err) console.log(err);
+            var modifiedimage = base64_encode('uploads/' + 'modified' + '.jpg');
+
+            res.render('result', { modified: modifiedimage });
+        });
+
     });
 });  
 
-
+//http://www.hacksparrow.com/base64-encoding-decoding-in-node-js.html
 // function to encode file data to base64 encoded string
 function base64_encode(file) {
     // read binary data
