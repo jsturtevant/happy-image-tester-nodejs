df31ba0f92346c1ca22107a18d882cfddf69c7e5
James Sturtevant <jsturtevant@gmail.com>
2016-01-12 21:12:51 -0500

    got image writing working

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,45 @@
 var express = require('express');
 var router = express.Router();
+
+var multer = require('multer');
+//var storage = multer.memoryStorage()
+var upload = multer({ dest: 'uploads/' });
 var oxford = require('project-oxford');
+var fs = require('fs')
+  , gm = require('gm');
+
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
 });
 
-router.get('/result', function(req, res, next) {
-  res.render('result', { title: 'Express' });
+router.get('/result', function (req, res, next) {
+    res.render('result', { title: 'Express' });
 });
 
-router.post('/upload', function(req, res, next) {
+router.post('/upload', upload.single('thumbnail'), function (req, res, next) {
     var client = new oxford.Client('b38409352b284f459b5e60a3e9eb4fcd');
-    
+
     client.emotion.analyzeEmotion({
-        url: 'http://www.jamessturtevant.com/assets/profile-low-res.jpg',
+        path: req.file.path,
     }).then(function (response) {
         console.log(response);
-        
-        res.render('result', { title: 'Express' });
-    });  
-    
-  
-});
+      
+        gm(req.file.path)
+            .stroke("red", 7)
+            .fill("#ffffffbb")
+            .drawRectangle(60, 10, 70, 20)
+            .write('uploads/resize.jpg', function (err) {
+            if (err) console.log(err);
+            
+            res.render('result', { title: 'Express' });
+            });
+       
+    });
+});  
+
 
 module.exports = router;
